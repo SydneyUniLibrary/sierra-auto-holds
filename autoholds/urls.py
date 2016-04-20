bb31224de8998782f18a6f67b8c8bcf6eca1520a
Jim Nicholls <jim.nicholls@sydney.edu.au>
2016-04-20 18:26:35 +1000

    Copyright and license the project

--- autoholds/urls.py
+++ autoholds/urls.py
@@ -1,3 +1,21 @@
+# Copyright 2016 Susan Bennett, David Mitchell, Jim Nicholls
+#
+# This file is part of AutoHolds.
+#
+# AutoHolds is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# AutoHolds is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with AutoHolds.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """autoholds URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,6 +31,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import include, url
 from django.contrib import admin
 from patron import views as patron_views
