4e44d72d6ab9ab652a12668c72fb0a42b4ef7451
Jim Nicholls <jim.nicholls@sydney.edu.au>
2016-04-21 15:40:49 +1000

    Refactor autoholds.py and make it more resilient.

--- staff/management/commands/autoholds.py
+++ staff/management/commands/autoholds.py
@@ -16,7 +16,7 @@
 # along with AutoHolds.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from datetime import datetime, timezone
+from datetime import datetime
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -30,17 +30,21 @@ class Command(BaseCommand):
 
     help = 'Discover new bib records and place auto-holds on them'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sierra_api = None
+
     def handle(self, *args, **options):
         datetime_last_run = make_aware(datetime(2016, 3, 4))  # TODO: Track and retrieve last run date/time
         datetime_now = now()
 
-        sierra_api = SierraApi_v2.login(
+        self.sierra_api = SierraApi_v2.login(
                 settings.SIERRA_API.base_url,
                 settings.SIERRA_API.client_key,
                 settings.SIERRA_API.client_secret
         )
 
-        new_bibs = self._get_new_bibs(sierra_api, datetime_last_run, datetime_now)
+        new_bibs = self._get_new_bibs(datetime_last_run, datetime_now)
         # new_bibs = self._get_bibs(sierra_api, 1000001)
         self.stdout.write(
             'Found {} new bib records created between {} and {}'.format(
@@ -48,64 +52,88 @@ class Command(BaseCommand):
             )
         )
         for bib in new_bibs:
-            bib_rec_num = bib['id']
-            bib_author = bib['author']
-            if not bib_author:
+            try:
+                self._process_bib(bib)
+            except Exception as e:
                 self.stdout.write(
-                    'Skipping .b{}a because it has no author'.format(bib_rec_num),
-                    style_func=self.style.NOTICE
+                    'An error occurred while processing .b{}a: {}'.format(
+                        bib.get('id', '?'), e
+                    ),
+                    style_func=self.style.ERROR
                 )
-                continue
-            bib_material_type_code = bib['materialType']['code']
-            bib_language_code = bib['lang']['code']
-            regs = Registration.objects.filter(
-                author__name__iexact=bib_author,
-                format__code=bib_material_type_code,
-                format__active=True,
-                language__code=bib_language_code,
-                language__active=True
-            ).order_by('hold_queue_order')
+
+    def _process_bib(self, bib):
+        bib_rec_num = bib['id']
+        if bib_rec_num == '4862252':
+            raise Exception('Deliberate test of _process_bib going bang')
+        bib_author = bib['author']
+        if not bib_author:
             self.stdout.write(
-                'Found {} registrations for .b{}a, format {} and language {}'.format(
-                    len(regs), bib_rec_num, bib_material_type_code, bib_language_code
+                'Skipping .b{}a because it has no author'.format(bib_rec_num),
+                style_func=self.style.NOTICE
+            )
+            return
+        bib_material_type_code = bib['materialType']['code']
+        bib_language_code = bib['lang']['code']
+        regs = Registration.objects.filter(
+            author__name__iexact=bib_author,
+            format__code=bib_material_type_code,
+            format__active=True,
+            language__code=bib_language_code,
+            language__active=True
+        ).order_by('hold_queue_order')
+        self.stdout.write(
+            'Found {} registrations for .b{}a, format {} and language {}'.format(
+                len(regs), bib_rec_num, bib_material_type_code, bib_language_code
+            )
+        )
+        for reg in regs:
+            try:
+                self._place_hold(bib_rec_num, reg)
+            except Exception as e:
+                self.stdout.write(
+                    'An error occurred while processing registration id {} for .b{}a: {}'.format(
+                        reg.id, bib_rec_num, e
+                    ),
+                    style_func=self.style.ERROR
+                )
+        if len(regs) > 1:
+            first_reg = regs[0]
+            first_reg.move_to_last_in_hold_queue_order()
+            self.stdout.write(
+                'Moved .p{}a to bottom of queue for .b{}a, format {} and language {}. New position is {}'.format(
+                    first_reg.patron.patron_record_number, bib_rec_num, bib_material_type_code,
+                    bib_language_code, first_reg.hold_queue_order
                 )
             )
-            for reg in regs:
-                patron_rec_num = reg.patron.patron_record_number
-                pickup_location = reg.patron.pickup_location.code
-                try:
-                    sierra_api.patrons_place_hold(patron_rec_num, 'b', bib_rec_num, pickup_location)
-                    self.stdout.write(
-                        'Successfully added hold on .b{}a for .p{}a with pickup at {} (registration id {})'.format(
-                            bib_rec_num, patron_rec_num, pickup_location, reg.id
-                        ),
-                        style_func=self.style.SUCCESS
-                    )
-                except SierraApiError as e:
-                    self.stdout.write(
-                        'Failed to add hold on .b{}a for .p{}a for (registration id {}): {}'.format(
-                            bib_rec_num, patron_rec_num, reg.id, e
-                        ),
-                        style_func=self.style.WARNING
-                    )
-            if len(regs) > 1:
-                first_reg = regs[0]
-                first_reg.move_to_last_in_hold_queue_order()
-                self.stdout.write(
-                    'Moved .p{}a to bottom of queue for .b{}a, format {} and language {}. New position is {}'.format(
-                        first_reg.patron.patron_record_number, bib_rec_num, bib_material_type_code,
-                        bib_language_code, first_reg.hold_queue_order
-                    )
-                )
 
-    def _get_bibs(self, sierra_api, *bib_ids):
+    def _place_hold(self, bib_rec_num, registration):
+        patron_rec_num = registration.patron.patron_record_number
+        pickup_location = registration.patron.pickup_location.code
+        try:
+            self.sierra_api.patrons_place_hold(patron_rec_num, 'b', bib_rec_num, pickup_location)
+            self.stdout.write(
+                'Successfully placed hold on .b{}a for .p{}a with pickup at {} (registration id {})'.format(
+                    bib_rec_num, patron_rec_num, pickup_location, registration.id
+                ),
+                style_func=self.style.SUCCESS
+            )
+        except SierraApiError as e:
+            self.stdout.write(
+                'Failed to place hold on .b{}a for .p{}a (registration id {}): {}'.format(
+                    bib_rec_num, patron_rec_num, registration.id, e
+                ),
+                style_func=self.style.WARNING
+            )
+
+    def _get_bibs(self, *bib_ids):
         result = list()
         for bib_id in bib_ids:
-            result.append(sierra_api.bibs_get_for_id(bib_id, fields='id,author,materialType,lang'))
+            result.append(self.sierra_api.bibs_get_for_id(bib_id, fields='id,author,materialType,lang'))
         return result
 
-    def _get_new_bibs(self, sierra_api, created_date_from, created_date_to):
-        result = sierra_api.bibs_get(
+    def _get_new_bibs(self, created_date_from, created_date_to):
+        result = self.sierra_api.bibs_get(
             created_date={'range_from': created_date_from, 'range_to': created_date_to},
             fields='id,author,materialType,lang',
             deleted=False,
